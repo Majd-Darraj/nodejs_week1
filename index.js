d801133339354cef690f92b363f5080cb873c78d
Majd <majd.darraj@hotmail.com>
2018-06-18 12:04:03 +0200

    Join concatenated strings | Find word by regex | Excution timer | Step-1 DONE

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 // modules
 const util = require('util');
 const fs = require('fs');
+const os = require('os');
+const perf = require('execution-time')();
+
 
 // assign promisify (read file from system)
 const readFileP = util.promisify(fs.readFile);
+
+// measure excution time (START)
+perf.start();
 
 // define files variables (promises)
 const chapterOne = readFileP('./story_chapter_1.txt', 'utf8');
 const chapterTwo = readFileP('./story_chapter_2.txt', 'utf8');
 const chapterThree = readFileP('./story_chapter_3.txt', 'utf8');
 
-// excute two promises (chapters)
+// excute three promises (chapters)
 Promise.all([chapterOne, chapterTwo, chapterThree])
 	.then((data) => {
-				// console.log(data);
+		// console.log(data);
 		// concatenate promises output to variable
-		const fullStory = [data[0]].concat(data[1]);
-		// console.log(fullStory);
+		const fullStory = [data[0]].concat(data[1]).join();
+		let wordCount = fullStory.match(/mouse/gi).length;
+	
+		console.log(`2- The word "mouse" repeated ${wordCount} times in this story.\n\n`);
+		console.log(`${fullStory}\n\n`);
+		console.log(`**** It took ${asyncTimerEnd} milliseconds to excute your code. *****\n\n`);
 
-		// console.log("THE STORY IS", fullStory);
-		// write concatenated promises to file
-		fs.writeFile('story.txt', fullStory);
+
 	})
 	.catch((err) => {
 		throw err;
 });
 
+
+// measure excution time (END)
+let asyncTimerEnd = perf.stop().time;
 
 
 
